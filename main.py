97060dcd2ceb4508e2df40028362a1ef1325164d
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2026-02-18 17:13:20 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+#1-misol
+numbers = (15, 8, 22, 3, 19)
+print(numbers)
+print(max(numbers))
+
+#2-misol
+numbers = (4, 9, 16, 25, 36)
+print(numbers)
+if 16 in numbers:
+    print(16 in numbers)
+else:
+    print(16 not in numbers)
+
+
+#3-misol
+numbers = (1, 2, 3, 4, 5)
+
+numbers_list = list(numbers)[::-1]
+print(numbers_list)
+
+
+4-misol
+t1 = (1, 2, 3)
+t2 = (4, 5, 6)
+
+print(t1)
+print(t2)
+print(t1 + t2)
+
+#5-misol
+data = ("Ali", 25, "Toshkent", "Dasturchi")
+print(data[2])
